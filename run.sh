a7f84059c0b5bbdc0ada870007e5e65cb2352ce3
Harsh Kumar <harshvardhkumar@gmail.com>
2020-08-19 14:12:53 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,59 @@
+./run_champsim.sh bimodal-next_line-next_line-berti-next_line2-lru-1core 1 15 621.wrf_s-8100B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-team_12-next_line2-lru-1core 1 15 621.wrf_s-8100B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-perc_bertii-next_line2-lru-1core 1 15 621.wrf_s-8100B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-berti-next_line2-lru-1core 1 15 625.x264_s-12B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-team_12-next_line2-lru-1core 1 15 625.x264_s-12B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-perc_bertii-next_line2-lru-1core 1 15 625.x264_s-12B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-berti-next_line2-lru-1core 1 15 627.cam4_s-490B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-team_12-next_line2-lru-1core 1 15 627.cam4_s-490B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-perc_bertii-next_line2-lru-1core 1 15 627.cam4_s-490B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-berti-next_line2-lru-1core 1 15 638.imagick_s-824B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-team_12-next_line2-lru-1core 1 15 638.imagick_s-824B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-perc_bertii-next_line2-lru-1core 1 15 638.imagick_s-824B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-berti-next_line2-lru-1core 1 15 641.leela_s-149B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-team_12-next_line2-lru-1core 1 15 641.leela_s-149B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-perc_bertii-next_line2-lru-1core 1 15 641.leela_s-149B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-berti-next_line2-lru-1core 1 15 644.nab_s-7928B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-team_12-next_line2-lru-1core 1 15 644.nab_s-7928B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-perc_bertii-next_line2-lru-1core 1 15 644.nab_s-7928B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-berti-next_line2-lru-1core 1 15 648.exchange2_s-387B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-team_12-next_line2-lru-1core 1 15 648.exchange2_s-387B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-perc_bertii-next_line2-lru-1core 1 15 648.exchange2_s-387B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-berti-next_line2-lru-1core 1 15 649.fotonik3d_s-1B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-team_12-next_line2-lru-1core 1 15 649.fotonik3d_s-1B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-perc_bertii-next_line2-lru-1core 1 15 641.fotonik3d_s-1B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-berti-next_line2-lru-1core 1 15 654.roms_s-1007B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-team_12-next_line2-lru-1core 1 15 654.roms_s-1007B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-perc_bertii-next_line2-lru-1core 1 15 654.roms_s-1007B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-berti-next_line2-lru-1core 1 15 657.xz_s-56B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-team_12-next_line2-lru-1core 1 15 657.xz_s-56B.champsimtrace.xz
+
+./run_champsim.sh bimodal-next_line-next_line-perc_bertii-next_line2-lru-1core 1 15 657.xz_s-56B.champsimtrace.xz
